9b8b2e750e64aa84663a521b905d42c5fb615ee5
Goswami21012007 <82029506+Goswami21012007@users.noreply.github.com>
2021-06-15 20:25:53 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,21 @@
+class Block {
+    constructor(x,y,width,height) {
+      var options = {
+        friction:1.0,
+        density:0.04,
+        restitution:0.8,
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      push()
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("yellow");
+      rect(pos.x, pos.y, this.width, this.height);
+      pop()
+    }
+  };
